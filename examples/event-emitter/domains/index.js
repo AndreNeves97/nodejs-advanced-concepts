cc36f525d3f6a4495b835f5f16dc64fc2ccef71a
André Neves <andreneves3@gmail.com>
2022-11-28 23:59:06 -0300

    chore: remove "recipes" resource

--- examples/event-emitter/domains/index.js
+++ examples/event-emitter/domains/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const domains = [
-  { path: '/recipes', module: require('./recipes') },
   { path: '/orders', module: require('./orders') },
 ];
 
